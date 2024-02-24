8c2140a0f21856962121eb3bf10bd20c9595b17e
lillianassefa <lillianalehegn123@gmail.com>
2024-02-24 22:20:56 +0300

    api integration

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,10 +2,9 @@ from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
-import os
 from generator import create_conversational_chain
 from retriver import create_vectorstore, create_llm
-
+import os
 
 app = FastAPI()
 
@@ -17,18 +16,24 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class AnswerResponse(BaseModel):
     answer: str
 
+
 llm = create_llm()
-vectorstore = create_vectorstore()
 
+conversation_chain = None
 
-conversation_chain = create_conversational_chain(llm, vectorstore)
+# @app.on_event("startup")
+# async def startup_event():
+#     global conversation_chain
+#     vectorstore_path = "/path/to/vectorstore"
+#     conversation_chain = create_conversational_chain(llm, vectorstore_path)
+
 
 @app.post("/submit_text/")
 def submit_text(text: str = Form(...)):
-    
     result = conversation_chain({"question": text})
     answer = result["answer"]
     return {"answer": answer}
@@ -36,10 +41,15 @@ def submit_text(text: str = Form(...)):
 
 @app.post("/submit_file/")
 async def submit_file(file: UploadFile = File(...)):
+    file_path = f"/home/lillian/Documents/TenAcademy/week11/Contract-AI-Chatbot/backend/data/{file.filename}"
+    create_vectorstore(file_path)
     contents = await file.read()
-    result = conversation_chain({"file_contents": contents})
-    answer = result["answer"]
-    return {"answer": answer}
+    with open(file_path, "wb") as f:
+        f.write(contents)
+
+    with open(file_path, "rb") as f:
+        file_contents = f.read().decode()  
+    return {"file_contents": file_contents}
 
 if __name__ == "__main__":
     import uvicorn
--- backend/src/retriver.py
+++ backend/src/retriver.py
@@ -1,4 +1,4 @@
-from langchain_community.vectorstores import FAISS
+from langchain.vectorstores import FAISS
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
@@ -6,9 +6,8 @@ from dotenv import load_dotenv
 import os
 from langchain_openai import ChatOpenAI
 
-def create_vectorstore():
-    load_dotenv()
-    txt_file_path = os.getenv("TXT_FILE_PATH")  
+def create_vectorstore(txt_file_path):
+    # txt_file_path = "/home/lillian/Documents/TenAcademy/week11/Contract-AI-Chatbot/backend/data/contract.txt"
     loader = TextLoader(file_path=txt_file_path, encoding="utf-8")
     data = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=5100, chunk_overlap=200)
@@ -18,6 +17,7 @@ def create_vectorstore():
     return vectorstore
 
 def create_llm():
+    load_dotenv()
     api_key = os.getenv("OPENAI_API_KEY") 
     llm = ChatOpenAI(temperature=0.7, model_name="gpt-4", max_tokens=500, api_key=api_key)
     return llm
